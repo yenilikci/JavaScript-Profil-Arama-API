0c66eedf010cda563474dd647f745bc91f19a927
yenilikci <melihcelik017@outlook.com>
2020-08-27 16:08:36 +0300

    Profil Bilgilerinin Sorgulanması

---
+++ profile.js
@@ -0,0 +1,16 @@
+class Profile {
+    constructor() {
+        this.clientid = '';
+        this.clientSecret = '';
+    }
+
+    async getProfile(username){
+        const profileResponse = await fetch(`https://jsonplaceholder.typicode.com/users?username=${username}`);
+
+        const profile = await profileResponse.json();
+
+        return{
+            profile
+        }
+    }
+};
